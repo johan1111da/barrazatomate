422d907bbcd9df8ec1a40c9b8051ffd446c50317
johan1111da <132007311+johan1111da@users.noreply.github.com>
2024-07-09 22:20:58 -0500

    Update Conexion.py

--- app/Conexion.py
+++ app/Conexion.py
@@ -1,12 +1,13 @@
+import os
 from sqlalchemy import create_engine
-
 from sqlalchemy.orm import sessionmaker
 from sqlalchemy.orm import declarative_base
 
-DATABASE_URL = "mysql+mysqlconnector://root:@localhost:3306/crm"
+# Asegúrate de configurar esta variable de entorno en el entorno de Render o donde estés desplegando tu aplicación
+DATABASE_URL = os.getenv("DATABASE_URL", "mysql://u90tcvps80ln32im:IE6lHiP3qACiQ0buKfNg@bbdqxdh6h22vemwb2whz-mysql.services.clever-cloud.com:3306/bbdqxdh6h22vemwb2whz")
 
 engine = create_engine(DATABASE_URL)
 
-SessionLocal = sessionmaker(autocommit=False,autoflush=False,bind=engine)
+SessionLocal = sessionmaker(autocommit=False, autoflush=False, bind=engine)
 
 Base = declarative_base()
