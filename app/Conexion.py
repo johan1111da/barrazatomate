01617bac6f2ad554d3c54988094a0fd5e5727491
johan1111da <132007311+johan1111da@users.noreply.github.com>
2024-07-09 22:41:18 -0500

    Update Conexion.py

--- app/Conexion.py
+++ app/Conexion.py
@@ -4,7 +4,7 @@ from sqlalchemy.orm import sessionmaker
 from sqlalchemy.orm import declarative_base
 
 # Asegúrate de configurar esta variable de entorno en el entorno de Render o donde estés desplegando tu aplicación
-DATABASE_URL = os.getenv("DATABASE_URL", "mysql://u90tcvps80ln32im:IE6lHiP3qACiQ0buKfNg@bbdqxdh6h22vemwb2whz-mysql.services.clever-cloud.com:3306/bbdqxdh6h22vemwb2whz")
+DATABASE_URL = os.getenv("DATABASE_URL", "mysql://uzononyj9ilm10zt:KsNqXc83TPaI4QHIQkLl@b87dlp04louefhdei4wx-mysql.services.clever-cloud.com:3306/b87dlp04louefhdei4wx")
 
 engine = create_engine(DATABASE_URL)
 
